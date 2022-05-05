caa664633d457862d60f3416cf975c24b87ed297
LuizFelipe86 <>
2022-05-05 14:51:09 +0000

    novo bemvindo.cs

--- 2022_1_POO/Aula05/Bemvindo.cs
+++ 2022_1_POO/Aula05/Bemvindo.cs
@@ -4,6 +4,6 @@ class Program {
   public static void Main(){
     Console.WriteLine("Digite o seu nome");
     string s = Console.ReadLine();
-    Console.WriteLine("Olá, " + s);
+    Console.WriteLine($"Olá, {s}");
   }
 }
